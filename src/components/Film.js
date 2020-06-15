f95b7bf6a586caabcec7be0874d3693886db4303
Teo <43215755+teokigi@users.noreply.github.com>
2020-06-15 16:20:19 +0100

    href
    quick tweak of href links to make them work

--- src/components/Film.js
+++ src/components/Film.js
@@ -4,7 +4,7 @@ class Film extends Component{
     render(){
         return(
             <li className="film">
-                <h3><a href="{this.props.url}">{this.props.name}</a></h3>
+                <h3><a href={this.props.url}>{this.props.name}</a></h3>
             </li>);
     }
 }
